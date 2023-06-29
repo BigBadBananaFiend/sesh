a24af05e97237039412ccf184b7ebfd0b0c72d90
BigBadBananaFiend <92824002+BigBadBananaFiend@users.noreply.github.com>
2023-06-29 09:57:06 +0200

    chore: add timeouts

--- robot/tests/createSession.spec.ts
+++ robot/tests/createSession.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { test } from "@playwright/test";
 import { Sesh } from "../pom/sesh.pom";
 import { BASE_URL, BITCOIN, PREMIER_LEAUGE } from "../constants";
 
@@ -7,9 +7,11 @@ test("Create session", async ({ page }) => {
   await page.goto(BASE_URL);
 
   await sesh.articleOpen.nth(0).click();
+  await page.waitForRequest("https://web-writer.alfa.smartlook.cloud/**");
   await sesh.dialogHeadline.click();
   await sesh.dialogAddToFavorites.click();
   await sesh.dialogClose.click();
+  await page.waitForTimeout(10000);
 
   await sesh.topbarSearchNavButton.click();
   await sesh.searchInput.type(BITCOIN);
@@ -17,6 +19,7 @@ test("Create session", async ({ page }) => {
   await sesh.dialogAddToFavorites.click();
   await sesh.dialogClose.click();
   await sesh.searchInput.clear();
+  await page.waitForTimeout(10000);
 
   await sesh.searchInput.type(PREMIER_LEAUGE);
   await page.waitForRequest("https://newsapi.org/**");
@@ -24,9 +27,9 @@ test("Create session", async ({ page }) => {
   await sesh.dialogAddToFavorites.click();
   await sesh.dialogClose.click();
   await sesh.topbarFavoritesNavButton.click();
+  await page.waitForTimeout(10000);
 
   await sesh.favoritesHeadline.click();
   await sesh.articleOpen.nth(0).click();
-  const href = await sesh.articleHref.nth(0).getAttribute("href");
-  await page.goto(href ?? BASE_URL);
+  await page.waitForTimeout(10000);
 });
