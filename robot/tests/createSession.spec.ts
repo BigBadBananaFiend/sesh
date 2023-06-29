febbd2fae9710e550b4668c410d64d6b7a399043
BigBadBananaFiend <92824002+BigBadBananaFiend@users.noreply.github.com>
2023-06-29 15:52:34 +0200

    chore: increase timeouts

--- robot/tests/createSession.spec.ts
+++ robot/tests/createSession.spec.ts
@@ -3,6 +3,7 @@ import { Sesh } from "../pom/sesh.pom";
 import { BASE_URL, BITCOIN, PREMIER_LEAUGE } from "../constants";
 
 test("Create session", async ({ page }) => {
+  test.slow();
   const sesh = new Sesh(page);
   await page.goto(BASE_URL);
 
@@ -16,32 +17,52 @@ test("Create session", async ({ page }) => {
   await initWait();
 
   await sesh.articleOpen.nth(0).click();
+  await page.waitForTimeout(1000);
   await sesh.dialogHeadline.click();
+  await page.waitForTimeout(1000);
   await sesh.dialogAddToFavorites.click();
+  await page.waitForTimeout(1000);
   await sesh.dialogCopyText.click();
+  await page.waitForTimeout(1000);
   await sesh.dialogClose.click();
+  await page.waitForTimeout(1000);
 
   await sesh.topbarFavoritesNavButton.click();
+  await page.waitForTimeout(1000);
   await expect(sesh.articleContainer).toHaveCount(1);
+  await page.waitForTimeout(1000);
   await sesh.topbarSearchNavButton.click();
 
   await sesh.searchInput.fill(BITCOIN);
+  await page.waitForTimeout(1000);
   await expect(sesh.articleContainer.first()).toBeVisible();
+  await page.waitForTimeout(1000);
   await sesh.articleOpen.first().click();
+  await page.waitForTimeout(1000);
   await sesh.dialogAddToFavorites.click();
+  await page.waitForTimeout(1000);
   await sesh.dialogClose.click();
+  await page.waitForTimeout(1000);
 
   await sesh.topbarFavoritesNavButton.click();
+  await page.waitForTimeout(1000);
   await expect(sesh.articleContainer).toHaveCount(2);
+  await page.waitForTimeout(1000);
   await sesh.topbarSearchNavButton.click();
+  await page.waitForTimeout(1000);
 
   await sesh.searchInput.fill(PREMIER_LEAUGE);
+  await page.waitForTimeout(1000);
   await expect(sesh.articleContainer.first()).toBeVisible();
+  await page.waitForTimeout(1000);
   await sesh.articleOpen.nth(0).click();
+  await page.waitForTimeout(1000);
   await sesh.dialogAddToFavorites.click();
+  await page.waitForTimeout(1000);
   await sesh.dialogClose.click();
 
   await sesh.topbarFavoritesNavButton.click();
+  await page.waitForTimeout(1000);
   await expect(sesh.articleContainer).toHaveCount(3);
 
   await sesh.articleAddToFavorites.first().click();
