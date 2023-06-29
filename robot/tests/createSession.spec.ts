f52e6a903efba49b6aaac268ece51cae4505c258
BigBadBananaFiend <92824002+BigBadBananaFiend@users.noreply.github.com>
2023-06-29 16:06:40 +0200

    chore: switch api keys + slowMo

--- robot/tests/createSession.spec.ts
+++ robot/tests/createSession.spec.ts
@@ -7,62 +7,42 @@ test("Create session", async ({ page }) => {
   const sesh = new Sesh(page);
   await page.goto(BASE_URL);
 
-  const initWait = async () =>
-    page.evaluate(() => {
-      return new Promise((res) => {
-        window.addEventListener("forward", res);
-      });
-    });
+  // const initWait = async () =>
+  //   page.evaluate(() => {
+  //     return new Promise((res) => {
+  //       window.addEventListener("forward", res);
+  //     });
+  //   });
 
-  await initWait();
+  // await initWait();
 
   await sesh.articleOpen.nth(0).click();
-  await page.waitForTimeout(1000);
   await sesh.dialogHeadline.click();
-  await page.waitForTimeout(1000);
   await sesh.dialogAddToFavorites.click();
-  await page.waitForTimeout(1000);
   await sesh.dialogCopyText.click();
-  await page.waitForTimeout(1000);
   await sesh.dialogClose.click();
-  await page.waitForTimeout(1000);
 
   await sesh.topbarFavoritesNavButton.click();
-  await page.waitForTimeout(1000);
   await expect(sesh.articleContainer).toHaveCount(1);
-  await page.waitForTimeout(1000);
   await sesh.topbarSearchNavButton.click();
 
   await sesh.searchInput.fill(BITCOIN);
-  await page.waitForTimeout(1000);
   await expect(sesh.articleContainer.first()).toBeVisible();
-  await page.waitForTimeout(1000);
   await sesh.articleOpen.first().click();
-  await page.waitForTimeout(1000);
   await sesh.dialogAddToFavorites.click();
-  await page.waitForTimeout(1000);
   await sesh.dialogClose.click();
-  await page.waitForTimeout(1000);
 
   await sesh.topbarFavoritesNavButton.click();
-  await page.waitForTimeout(1000);
   await expect(sesh.articleContainer).toHaveCount(2);
-  await page.waitForTimeout(1000);
   await sesh.topbarSearchNavButton.click();
-  await page.waitForTimeout(1000);
 
   await sesh.searchInput.fill(PREMIER_LEAUGE);
-  await page.waitForTimeout(1000);
   await expect(sesh.articleContainer.first()).toBeVisible();
-  await page.waitForTimeout(1000);
   await sesh.articleOpen.nth(0).click();
-  await page.waitForTimeout(1000);
   await sesh.dialogAddToFavorites.click();
-  await page.waitForTimeout(1000);
   await sesh.dialogClose.click();
 
   await sesh.topbarFavoritesNavButton.click();
-  await page.waitForTimeout(1000);
   await expect(sesh.articleContainer).toHaveCount(3);
 
   await sesh.articleAddToFavorites.first().click();
--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const API_KEY = "fd3a951e7e3f491895a37b08fb232436";
+export const API_KEY = "c00d6a6ad74640938a80c8be46f6d5c4";
 export const IMAGE_PLACEHOLDER =
   "https://i.insider.com/5ba2a042e199f336008b60ba?width=700";
